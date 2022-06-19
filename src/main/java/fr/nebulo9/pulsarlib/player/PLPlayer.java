493a750446dc5ab263655e6cf573c71fc5c7c394
Nebulo9 <matdessauv@gmail.com>
2022-06-19 23:40:50 +0200

    Adding a custom class for player handling

---
+++ src/main/java/fr/nebulo9/pulsarlib/player/PLPlayer.java
@@ -0,0 +1,25 @@
+package fr.nebulo9.pulsarlib.player;
+
+import com.google.gson.annotations.Expose;
+
+import java.util.UUID;
+
+public class PLPlayer {
+    protected final UUID uuid;
+
+    @Expose(serialize = false)
+    protected final String name;
+
+    public PLPlayer(UUID uuid, String name) {
+        this.uuid = uuid;
+        this.name = name;
+    }
+
+    public UUID getUUID() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
